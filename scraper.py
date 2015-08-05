81baa4d18617740c0cd08b33155107d83d4517d9
genemau <genemau@users.noreply.github.com>
2015-08-05 13:31:17 +1000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -20,37 +20,15 @@ def getJobCount(url):
 
 
 urls = [
-      ['BA all', 'https://api.seek.com.au/v2/jobs/search?keywords=.net&graduateSearch=false&location=1002&isAreaUnspecified=false&salaryRange=100000-
-
-999999&salaryType=annual&dateRange=31']
-    , ['BA contract', 'https://api.seek.com.au/v2/jobs/search?keywords=.net&graduateSearch=false&location=1002&isAreaUnspecified=false&salaryRange=100000-
-
-999999&salaryType=annual&dateRange=31&worktype=244']
-    , ['BA perm', 'https://api.seek.com.au/v2/jobs/search?keywords=.net&graduateSearch=false&location=1002&isAreaUnspecified=false&salaryRange=100000-
-
-999999&salaryType=annual&dateRange=31&worktype=242']
-    ,  ['.net all', 'https://api.seek.com.au/v2/jobs/search?keywords=.net&graduateSearch=false&location=1002&isAreaUnspecified=false&salaryRange=100000-
-
-999999&salaryType=annual&dateRange=31']
-    , ['.net contract', 'https://api.seek.com.au/v2/jobs/search?keywords=.net&graduateSearch=false&location=1002&isAreaUnspecified=false&salaryRange=100000-
-
-999999&salaryType=annual&dateRange=31&worktype=244']
-    , ['.net perm', 'https://api.seek.com.au/v2/jobs/search?keywords=.net&graduateSearch=false&location=1002&isAreaUnspecified=false&salaryRange=100000-
-
-999999&salaryType=annual&dateRange=31&worktype=242']
-    , ['java all', 'https://api.seek.com.au/v2/jobs/search?
-
-keywords=java&hirerId=&hirerGroup=&page=1&classification=6281&subclassification=&graduateSearch=false&location=1002&salaryRange=100000-999999&salaryType=annual&dateRange=31']
-    , ['java contract', 'https://api.seek.com.au/v2/jobs/search?
-
-keywords=java&hirerId=&hirerGroup=&page=1&classification=6281&subclassification=&graduateSearch=false&location=1002&nation=&area=&isAreaUnspecified=false&worktype=244&salaryRange=10
-
-0000-999999&salaryType=annual&dateRange=31']
-    , ['java perm', 'https://api.seek.com.au/v2/jobs/search?
-
-keywords=java&hirerId=&hirerGroup=&page=1&classification=6281&subclassification=&graduateSearch=false&location=1002&nation=&area=&isAreaUnspecified=false&worktype=242&salaryRange=10
-
-0000-999999&salaryType=annual&dateRange=31']
+      ['BA all', 'https://api.seek.com.au/v2/jobs/search?keywords=.net&graduateSearch=false&location=1002&isAreaUnspecified=false&salaryRange=100000-999999&salaryType=annual&dateRange=31']
+    , ['BA contract', 'https://api.seek.com.au/v2/jobs/search?keywords=.net&graduateSearch=false&location=1002&isAreaUnspecified=false&salaryRange=100000-999999&salaryType=annual&dateRange=31&worktype=244']
+    , ['BA perm', 'https://api.seek.com.au/v2/jobs/search?keywords=.net&graduateSearch=false&location=1002&isAreaUnspecified=false&salaryRange=100000-999999&salaryType=annual&dateRange=31&worktype=242']
+    , ['.net all', 'https://api.seek.com.au/v2/jobs/search?keywords=.net&graduateSearch=false&location=1002&isAreaUnspecified=false&salaryRange=100000-999999&salaryType=annual&dateRange=31']
+    , ['.net contract', 'https://api.seek.com.au/v2/jobs/search?keywords=.net&graduateSearch=false&location=1002&isAreaUnspecified=false&salaryRange=100000-999999&salaryType=annual&dateRange=31&worktype=244']
+    , ['.net perm', 'https://api.seek.com.au/v2/jobs/search?keywords=.net&graduateSearch=false&location=1002&isAreaUnspecified=false&salaryRange=100000-999999&salaryType=annual&dateRange=31&worktype=242']
+    , ['java all', 'https://api.seek.com.au/v2/jobs/search?keywords=java&hirerId=&hirerGroup=&page=1&classification=6281&subclassification=&graduateSearch=false&location=1002&salaryRange=100000-999999&salaryType=annual&dateRange=31']
+    , ['java contract', 'https://api.seek.com.au/v2/jobs/search?keywords=java&hirerId=&hirerGroup=&page=1&classification=6281&subclassification=&graduateSearch=false&location=1002&nation=&area=&isAreaUnspecified=false&worktype=244&salaryRange=100000-999999&salaryType=annual&dateRange=31']
+    , ['java perm', 'https://api.seek.com.au/v2/jobs/search?keywords=java&hirerId=&hirerGroup=&page=1&classification=6281&subclassification=&graduateSearch=false&location=1002&nation=&area=&isAreaUnspecified=false&worktype=242&salaryRange=100000-999999&salaryType=annual&dateRange=31']
 ]
 
 extractedOn = datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')
@@ -58,10 +36,14 @@ extractedOn = datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')
 for url in urls:
 
     search = url[0]
+    cnt = getJobCount(url[1])
+
+    print search
+    print cnt
 
     # Save found data
     scraperwiki.sqlite.save(unique_keys=['extracted_on','search'], data={
         "extracted_on": extractedOn,
         "search": suburb,
-	"count": getJobCount(url[1])
+	"count": cnt
         })
